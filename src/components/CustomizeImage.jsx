445fa50d51ab48e1f233236dcda098febf19a6d4
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-04-05 16:20:36 +0200

    full function

--- src/components/CustomizeImage.jsx
+++ src/components/CustomizeImage.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const CustomizeImage = () => {
+export class CustomizeImage extends Component {
+  state = {
+    url: null,
+    size: null,
+  };
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({ [name]: value });
+  };
+
+  render() {
     return (
-        <div>
-            
+      <div>
+        <h1>Customize Image</h1>
+        <input id="url" name="url" type="text" onChange={this.handleChange} />
+        <div className="slide-container">
+          <input
+            type="range"
+            min="1"
+            max="200"
+            className="slider"
+            id="size"
+            name="size"
+            onChange={this.handleChange}
+          />
+          <h3>
+            {this.state.size} x {this.state.size} px
+          </h3>
         </div>
-    )
+
+        {this.state.url && (
+          <img
+            style={{
+              width: `${this.state.size}px`,
+              height: `${this.state.size}px`,
+            }}
+            src={this.state.url}
+            alt="url"
+          />
+        )}
+      </div>
+    );
+  }
 }
 
-export default CustomizeImage
+export default CustomizeImage;
--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Temperature = () => {
-    return (
-        <div>
+export class Temperature extends Component {
+
+    state = {
+        temperature: null,
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value
+        const name = event.target.name
+        this.setState({[name]: value}) 
+    }
+    
+    
+
+    render() {
+
+        return (
+            <div>
+            <h1>Temperature</h1>
+            <input name="temperature" id="temperature" type="number" onChange={this.handleChange}/>
             
-        </div>
-    )
+            {this.state.temperature <= 10 && <h1 className="cold">It's cold ❄️</h1>}
+            {this.state.temperature > 10 && this.state.temperature < 30 && <h1 className="warm">It's nice 🌼</h1>}
+            { this.state.temperature >= 30 &&<h1 className="hot">It's warm ☀️</h1>}
+                
+            </div>
+        )
+    }
 }
 
 export default Temperature
+
